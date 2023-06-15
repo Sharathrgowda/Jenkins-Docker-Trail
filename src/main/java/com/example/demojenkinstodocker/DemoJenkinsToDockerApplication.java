b415a823081e0780a4026988ea9cb725e3a686ca
EXCELINDIA\sharath.r <sharath.r@excelindia.com>
2023-06-15 12:39:21 +0530

    first commit

---
+++ src/main/java/com/example/demojenkinstodocker/DemoJenkinsToDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojenkinstodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsToDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsToDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demojenkinstodocker/DemoJenkinsToDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojenkinstodocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsToDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
